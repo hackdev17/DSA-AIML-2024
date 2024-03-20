e6f9597d7f7aaa12800b6001c9e07ebb91750623
hackdev17 <youremail@yourdomain.com>
2024-03-20 22:58:02 +0530

    final update

--- Program1.c
+++ Program1.c
@@ -10,52 +10,52 @@ typedef struct day{
     char *AD;
 }Day;
 
-void create(Day *calendar){
+void create(Day *cal){
         char *Days[]= \
                 {"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"};
         for(int i=0;i<7;i++){
-                calendar[i].day=strdup(Days[i]);
-                calendar[i].AD=(char *)malloc(bs*sizeof(char));
+                cal[i].day=strdup(Days[i]);
+                cal[i].AD=(char *)malloc(bs*sizeof(char));
         }
 }
 
-void read(Day *calendar){
+void read(Day *cal){
         for(int i=0;i<7;i++){
-                printf("Enter date for %s in dd/mm/yy : ",calendar[i].day);
-                scanf("%d%d%d",&calendar[i].d,&calendar[i].m,&calendar[i].y);
-                printf("Enter activity for %s : ",calendar[i].day);
+                printf("Enter date for %s in dd/mm/yy : ",cal[i].day);
+                scanf("%d%d%d",&cal[i].d,&cal[i].m,&cal[i].y);
+                printf("Enter activity for %s : ",cal[i].day);
                 while(getchar()!='\n')
                     ;
-                getline(&calendar[i].AD,&bs,stdin);
+                getline(&cal[i].AD,&bs,stdin);
         }
 }
 
-void display(Day *calendar){
+void display(Day *cal){
         printf("%-10s %-10s \t %s\n","Day","Date","Activity");
         for(int i=0;i<7;++i){
                 printf("%-10s %d/%d/%d \t %s\n", \
-                        calendar[i].day,calendar[i].d, \
-                        calendar[i].m,calendar[i].y, \
-                        calendar[i].AD
+                        cal[i].day,cal[i].d, \
+                        cal[i].m,cal[i].y, \
+                        cal[i].AD
                 );
         }
 }
 
 int main(){
-        Day *calendar=(Day *)malloc(7*sizeof(Day));
-        if(calendar==NULL){
+        Day *cal=(Day *)malloc(7*sizeof(Day));
+        if(cal==NULL){
                 fprintf(stderr,"Memory allocation failed\n");
                 return 1;
         }
 
-        create(calendar);
-        read(calendar);
-        display(calendar);
+        create(cal);
+        read(cal);
+        display(cal);
 
         for(int i=0;i<7;++i){
-                free(calendar[i].day);
-                free(calendar[i].AD);
+                free(cal[i].day);
+                free(cal[i].AD);
         }
-        free(calendar);
+        free(cal);
         return 0;
 }
